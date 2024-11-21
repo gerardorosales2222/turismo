ecf7c28c882683bee016f6797940fd889bfcc20f
gerardorosales2222 <gerardorosales2222@gmail.com>
2024-11-20 23:34:21 -0300

    Refactor - scripts en main.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)  ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![Adobe Illustrator](https://img.shields.io/badge/adobe%20illustrator-%23FF9A00.svg?style=for-the-badge&logo=adobe%20illustrator&logoColor=white) ![Figma](https://img.shields.io/badge/Figma-F24822?style=for-the-badge&logo=figma&logoColor=white)
 
-## Proyecto Turismo :hamburger:
+## Proyecto Turismo :airplane:
 
 Este proyecto es una pequeña muestra de lo que se puede hacer con HTML, CSS y JavaScript. Está aún en fase de pruebas. 💻<br><br>
 
@@ -9,9 +9,9 @@ Este proyecto es una pequeña muestra de lo que se puede hacer con HTML, CSS y J
 
 **¡A probarlo!**
 
-Si quieres ver este proyecto en acción, el [link](https://viajesyturismotucma.netlify.app/) está en la sección about o a continuación. ¡Buen provecho!
+Si quieres ver este proyecto en acción:
 
-[Visita mi página de hamburguesas](https://viajesyturismotucma.netlify.app/)
+[Visita mi página de turismo](https://viajesyturismotucma.netlify.app/)
 
 **¡Contribuciones bienvenidas!**
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,28 @@
-class Libro {
-    constructor(titulo, autor, edicion, precio, cantidadDisponible) {
-      this.titulo = titulo;
-      this.autor = autor;
-      this.edicion = edicion;
-      this.precio = precio;
-      this.cantidadDisponible = cantidadDisponible;
-    }
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const phone = document.getElementById('phone').value;
+  const residence = document.getElementById('residence').value;
+  const email = document.getElementById('email').value;
+
+  if (!name || !phone || !residence) {
+      alert('Por favor, completa todos los campos obligatorios.');
+      return;
   }
-  
-  class Biblioteca {
-    constructor() {
-      this.libros = [];
-    }
-  
-    agregarLibro(libro) {
-      this.libros.push(libro);
-    }
-  
-    venderLibro(titulo, cantidad) {
-      const libro = this.libros.find(l => l.titulo === titulo);
-      if (libro && libro.cantidadDisponible >= cantidad) {
-        libro.cantidadDisponible -= cantidad;
-        console.log(`Venta realizada: ${cantidad} copias de "${titulo}"`);
-      } else {
-        console.log(`No hay suficientes copias disponibles de "${titulo}"`);
-      }
-    }
-  
-    mostrarInformacionLibros() {
-      this.libros.forEach(libro => {
-        console.log(`Título: ${libro.titulo}, Autor: ${libro.autor}, Edición: ${libro.edicion}, Precio: $${libro.precio}, Cantidad Disponible: ${libro.cantidadDisponible}`);
-      });
-    }
+
+  if (!/^[0-9]{10}$/.test(phone)) {
+      alert('Por favor, ingresa un número de teléfono válido de 10 dígitos.');
+      return;
   }
-  
-  const bibliotecaEjemplo = new Biblioteca();
-  bibliotecaEjemplo.agregarLibro(new Libro('Libro 1', 'Autor 1', '1ª edición', 20, 10));
-  bibliotecaEjemplo.agregarLibro(new Libro('Libro 2', 'Autor 2', '2ª edición', 15, 5));
-  bibliotecaEjemplo.mostrarInformacionLibros();
-  
+
+  alert('Formulario enviado con éxito.');
+  setTimeout(function() {
+    window.location.href = "./index.html";
+}, 3000);
+});
+
+
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+}
